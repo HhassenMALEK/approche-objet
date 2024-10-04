7474b3f9b11e4badf79e0a20b43fb9ff35b785c9
Votre Nom <h.hassen.malek@gmail.com>
2024-10-04 15:04:34 +0200

    rectification code tp02

--- src/main/java/entites/TestPersonne.java
+++ src/main/java/entites/TestPersonne.java
@@ -7,10 +7,11 @@ import java.security.spec.RSAOtherPrimeInfo;
 public class TestPersonne {
     public static void main (String[] args){
 
-        Personne personne1 = new Personne("MALEK", "Hassen", new AdressePostale(12, "Rue du bocage", 44100, "Nantes"));
+        AdressePostale adr1 = new AdressePostale(12, "Rue du bocage", 44100, "Nantes");
+        AdressePostale adr2 = new AdressePostale(12, "Rue de la République", 85600, "Les Herbiers");
 
-        Personne personne2 = new Personne("MALEK", "Ilyes", new AdressePostale(12, "Rue de la République", 85600, "Les Herbiers"));
+        Personne personne1 = new Personne("MALEK", "Hassen", adr1);
 
-
+        Personne personne2 = new Personne("MALEK", "Ilyes", adr2);
     }
 }
